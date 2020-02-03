6f5536f194f4fce81da49dbb2437710169ff48ff
pablog <pablogarciamin@gmail.com>
2020-02-03 13:54:21 +0100

    primer paso

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca los datos:");
+		System.out.println("Nombre:");
+		String nombre=sc.nextLine();
+		System.out.println("Apellidos:");
+		String apellidos=sc.nextLine();
+		System.out.println("Fecha de nacimeinto:");
+		String fecha=sc.nextLine();
+		System.out.println("DNI:");
+		String dni=sc.nextLine();
+		System.out.println("Estudios previos:");
+		String estudios=sc.nextLine();
+		System.out.println("Teléfono:");
+		String telefono=sc.nextLine();
+		
+	}
+
+}
